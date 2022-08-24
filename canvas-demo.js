c37973e6fc950f8287ca3d7713f37c0ee5b35921
Luke Nemy <lukenemy7@gmail.com>
2022-08-24 09:29:09 -1000

    starting to animate along a Bezier curve

--- canvas-demo.js
+++ canvas-demo.js
@@ -2,26 +2,47 @@ const dC = document.getElementById('canvas-demo').getContext('2d');
 
 window.onload = () => {
 
-  dC.moveTo(10,10);
-  dC.lineTo(110,10);
+  // dC.moveTo(10,10);
+  // dC.lineTo(110,10);
 
-  dC.moveTo(10,50);
-  dC.lineTo(180, 50);
+  // dC.moveTo(10,50);
+  // dC.lineTo(180, 50);
 
-  dC.moveTo(10,100);
-  dC.lineTo(150,100);
+  // dC.moveTo(10,100);
+  // dC.lineTo(150,100);
 
-  dC.moveTo(10, 110);
-  dC.lineTo(160, 110);
+  // dC.moveTo(10, 110);
+  // dC.lineTo(160, 110);
 
 
-  dC.moveTo(10, 300);
-  dC.quadraticCurveTo(110, 255, 210, 300);
-  dC.moveTo(10, 300);
-  dC.quadraticCurveTo(110, 345, 210, 300)
+  // dC.moveTo(10, 300);
+  // dC.quadraticCurveTo(110, 255, 210, 300);
+  // dC.moveTo(10, 300);
+  // dC.quadraticCurveTo(110, 345, 210, 300)
 
-  dC.moveTo(132.5, 300);
-  dC.arc(110, 300, 22.5,0 ,2*Math.PI, false );
+  // dC.moveTo(132.5, 300);
+  // dC.arc(110, 300, 22.5,0 ,2*Math.PI, false );
 
-  dC.stroke();
+  // dC.stroke();
+
+
+  
 }
+
+//Coefficient calculations for control points Bezier Curve animation movement
+
+// var cx = 3 * (p1.x - p0.x)
+// var bx = 3 * (p2.x - p1.x) - cx;
+// var ax = p3.x - p0.x - cx -bx;
+
+// var cy = 3 * (p1.y - p0.y)
+// var by = 3 * (p2.y - p1.y) - cy;
+// var ay = p3.y - p0.y - cy -by;
+
+
+// t-value represents progress along the curve: 0 -> 1
+// higher t-value, the faster the animation will be
+// var xt = ax* (t*t*t) + bx*(t*t) + cx*t + p0.x;
+// var yt = ay* (t*t*t) + by*(t*t) + cy*t + p0.y;
+
+// var = ball = {x:0, y:0, speed:.01, t:0}
